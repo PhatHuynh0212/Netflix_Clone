b688826629089dde107c7838bf35d5ce2965441f
huynhthanhphat <thanhphat02122003@gmail.com>
2024-11-23 23:46:53 +0700

    add middleware check user

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import { ENV_VAR } from "./config/envVar.js";
 import { connectDB } from "./config/database.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
 const PORT = ENV_VAR.PORT;
 
 app.use(express.json()); // parse req.body
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server run at port", PORT);
